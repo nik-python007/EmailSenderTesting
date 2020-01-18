92d9d4ce1e7bc19e772a9eb76bbb75d662b2bfbe
dell <dell@DELL-PC>
2020-01-18 19:35:02 +0530

    Add project files.

---
+++ Testing2Identiy/Areas/Identity/Services/EmailSender.cs
@@ -0,0 +1,46 @@
+﻿using Microsoft.AspNetCore.Identity.UI.Services;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net;
+using System.Net.Mail;
+using System.Threading.Tasks;
+
+namespace Testing2Identiy.Areas.Identity.Services
+{
+    public class EmailSender : IEmailSender
+    {
+        public string host="smtp.gmail.com";
+        public int port=587;
+        public bool enableSSL=true;
+        public string userName;
+        public string password;
+
+        public EmailSender()
+        {
+            host = "smtp.google.com";
+            port = 465;
+            enableSSL = true;
+            userName = "ppbholu511@gmail.com";
+            password = "nikhil1212";
+        }
+       
+
+     
+
+        public async Task SendEmailAsync(string email,string subject,string message)
+        {
+
+            var client = new SmtpClient(host, port)
+            {
+                Credentials = new NetworkCredential(userName, password),
+                EnableSsl = enableSSL
+            };
+            await client.SendMailAsync(userName,email,subject,message);
+            //return client.SendMailAsync(
+            //    new MailMessage( userName, email, subject, message) { IsBodyHtml = true }
+            //);
+          
+        }
+    }
+}
---
+++ Testing2Identiy/Controllers/HomeController.cs
@@ -0,0 +1,57 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Diagnostics;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.Extensions.Configuration;
+using Testing2Identiy.Areas.Identity.Services;
+using Testing2Identiy.Models;
+
+namespace Testing2Identiy.Controllers
+{
+    public class HomeController : Controller
+    {
+        private IConfiguration configuration;
+
+        public HomeController(IConfiguration iconfig)
+        {
+            configuration = iconfig;
+
+        }
+        
+        public IActionResult Index()
+        {
+           
+            EmailSender emailSender = new EmailSender() ;
+            emailSender.SendEmailAsync("nikhilgondaliya31804@gmail.com", "Testing", "Hello");
+            return View();
+            
+        }
+
+        public IActionResult About()
+        {
+            ViewData["Message"] = "Your application description page.";
+
+            return View();
+        }
+
+        public IActionResult Contact()
+        {
+            ViewData["Message"] = "Your contact page.";
+
+            return View();
+        }
+
+        public IActionResult Privacy()
+        {
+            return View();
+        }
+
+        [ResponseCache(Duration = 0, Location = ResponseCacheLocation.None, NoStore = true)]
+        public IActionResult Error()
+        {
+            return View(new ErrorViewModel { RequestId = Activity.Current?.Id ?? HttpContext.TraceIdentifier });
+        }
+    }
+}
